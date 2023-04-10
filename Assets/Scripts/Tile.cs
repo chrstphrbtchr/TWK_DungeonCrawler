17936e4dcc14a3a12d55655c9ba52e5516680610
chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
2023-04-10 15:25:44 -0400

    Squashed commit of the following:
    commit f4d357822037af666aad8a1309b9aa7a16e192d6
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Mon Apr 10 15:21:25 2023 -0400
    
        It works!!!
    
    commit f55acbea4be01a3bc3e2aac0848abe958273ece7
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Mon Apr 10 12:44:30 2023 -0400
    
        Fixed multiple errors.
    
    commit ce9d49c09710202466cd0f1c4b9106ed6a5acaf0
    Author: chrstphrbtchr <chrstphrbtchr@users.noreply.github.com>
    Date:   Sun Apr 9 15:57:49 2023 -0400
    
        Update Tile.cs
    
        Debugging in the sun. SOMETHING is wrong with UpdateNeighborSuperpositions.
    
    commit 5abfd3c2e892e46202bcbc2f1d7eda53a811fcc5
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Sun Apr 9 13:14:55 2023 -0400
    
        Cleaned up Tile, some debugging in WFC.
    
        The bug should probably be in UpdateNeighborsSuperpositions...
    
    commit e4103e357930285bdaf89a0d8d7085778f620f94
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Sun Apr 9 12:20:37 2023 -0400
    
        Completely rethinking how I handle TileInfo.
    
    commit b454cedc38299f1157ce24680ba51bfa7981e037
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Sat Apr 8 19:46:26 2023 -0400
    
        JamesAcasterOnTGBBO.mov
    
        Started making it.
        Had a breakdown.
        Bon appétit.
    
    commit 163f35bdc258738ed4f8970aae6e8f468c388740
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Sat Apr 8 18:52:07 2023 -0400
    
        Updated Tile, TilesMaster, and WFC scripts.
    
        It (mostly) works (mostly)!
    
    commit 08d97bad7af1912aac001909f8c32df38517061a
    Author: chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
    Date:   Sat Apr 8 18:10:11 2023 -0400
    
        Overhaul of WFC algorithm.
    
        Rewrote methods pertaining to Wave Function Collapse.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,10 +7,11 @@ public class Tile : MonoBehaviour
     public short tileNum = 0;
     public int entropy;
     public bool collapsed = false;
+    public Vector2Int tileLoc;
 
     public List<short> superpositions = new List<short>() { 
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-    public List<short>[] neighborCandidates =
+    public List<short>[] possibleNeighboringSuperpositions =
     {
         new List<short>(){ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
         new List<short>(){ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
@@ -28,46 +29,7 @@ public class Tile : MonoBehaviour
     }
 
     public int GetEntropy() => entropy;
-    public void FindEntropy()
-    {
-        entropy = superpositions.Count;
-        /*
-        if(entropy <= 1)
-        {
-            CollapseTile();
-        }
-        */
-    }
-
-    /*
-    void RecalculateSuperpositions(List<short> newOnes)
-    {
-        bool changed = false;
-
-        for(int i = superpositions.Count; i > 0 ; i--)
-        {
-            
-            if (!newOnes.Contains(superpositions[i - 1]))
-            {
-                superpositions.RemoveAt(i - 1);
-                changed = true;
-            }
-        }
-
-        if (changed)
-        {
-            CalculatePossibleNeighbors();
-
-            for (int q = 0; q < neighbors.Length; q++)
-            {
-                if (neighbors[q] != null)
-                {
-                    neighbors[q].UpdateNeighborsSuperpositions((q + 2) % 4);
-                }
-            }
-        }
-    }
-    */
+    public void FindEntropy() => entropy = superpositions.Count;
 
     public void CollapseTile()
     {
@@ -75,10 +37,18 @@ public class Tile : MonoBehaviour
         {
             collapsed = true;
             TilesMaster.collapsedTiles++;
-
             List<short> oldSuperpositions = new List<short>();
             oldSuperpositions.Concat(superpositions);
+
             short choice = -1;
+
+            for (int a = superpositions.Count - 1; a >= 0; a--)
+            {
+                if (!DoubleCheckSuperposition(superpositions[a]))
+                {
+                    superpositions.RemoveAt(a);
+                }
+            }
 
             if (superpositions.Count > 0)
             {
@@ -93,42 +63,68 @@ public class Tile : MonoBehaviour
                 }
                 tileNum = tInfo.tileIndex;
                 choice = (short)tileNum;
+                /*
                 superpositions.Clear();
                 superpositions.Add(choice);
 
-                for(int i = 0; i < neighborCandidates.Length; i++)
+                for(int i = 0; i < possibleNeighboringSuperpositions.Length; i++)
                 {
-                    neighborCandidates[i] = TilesMaster.allTiles[choice].rules[i];
+                    possibleNeighboringSuperpositions[i].Clear();
+                    TileInfo thisTilesInfo = TilesMaster.allTiles[tileNum];
+
+                    for(int g = 0; g < TilesMaster.allTiles.Length; g++)
+                    {
+                        TileInfo thatTilesInfo = TilesMaster.allTiles[g];
+                        if(WFC.IsNeighborValid(thisTilesInfo.sockets, thatTilesInfo.sockets, i))
+                        {
+                            possibleNeighboringSuperpositions[i].Add((short)g);
+                            Debug.LogFormat("Added {0} to {1}'s NL#{2}", g, this.name, i);
+                        }
+                    }
                 }
+                */
             }
 
             if(choice >= 0)
             {
                 AssignSprite(choice);
-                UpdateNeighborsSuperpositions(true, null, -1);
+                List<short> failsafe = new List<short> { choice };
+                UpdateNeighborsSuperpositions(true, failsafe, -1);
                 TilesMaster.EvaluatePercentages(tileNum);
             }
             else
             {
-                Debug.LogWarningFormat("{0} has no choices!", this.name);
-                UpdateNeighborsSuperpositions(false, oldSuperpositions, -1);
+                Debug.LogWarningFormat("{0} has no choices! {1}", this.name, oldSuperpositions.Count);
                 collapsed = false;
+                TilesMaster.collapsedTiles--;
+
+                if (oldSuperpositions.Count == 0)
+                {
+                    AssignStartingSuperpositions(tileLoc.x, tileLoc.y);
+                    UpdateNeighborsSuperpositions(false, superpositions, -1);
+                }
+                else
+                {
+                    UpdateNeighborsSuperpositions(false, oldSuperpositions, -1);
+                }
             }
-            
         }
     }
 
     void AssignSprite(short s)
     {
-        this.name += " - " + s;
+        this.name = "Tile " + tileLoc.ToString() + " - " + s.ToString();
         WFC.ChangeTile(this, s);        // I HATE THIS. BUT, FOR A GAME JAM, I'LL LIVE...
         print("ASSIGNED " + s + " TILE!");
     }
 
     void UpdateNeighborsSuperpositions(bool onCollapse, List<short> newSuperpositions, short neighborIndexOfCaller)
     {
+        string qqq = (neighborIndexOfCaller >= 0 ? neighbors[neighborIndexOfCaller].name : "Collapse method");
+        Debug.LogFormat("<color=green>{0} is updating neighbors w/ their {1} superpositions. Called from {2}.</color>", this.name, newSuperpositions.Count, qqq);
         if (!onCollapse)
         {
+            Debug.LogFormat("SUPERPOSITIONS: {0} / {1}!", superpositions.Count, newSuperpositions.Count);
             int oldSuperpositionsCount = superpositions.Count;
 
             for(int i = superpositions.Count - 1; i >= 0; i--)
@@ -144,19 +140,25 @@ public class Tile : MonoBehaviour
                 // There has been a change, so update our neighboring superpositions and call this method on our neighbors EXCEPT nIofC
                 if (superpositions.Count == 0)
                 {
+                    Debug.LogWarning("No superpositions for " + this.name);
                     if (collapsed)
                     {
                         collapsed = false;
                         TilesMaster.collapsedTiles--;
                         TilesMaster.EvaluatePercentages(tileNum, true);
                     }
-                    
+
                     superpositions.Concat(newSuperpositions);
+                    AssignStartingSuperpositions(tileLoc.x, tileLoc.y);
+                    UpdateNeighborsSuperpositions(false, superpositions, -1);
                 }
             }
             else
             {
-                CalculatePossibleNeighbors(neighborIndexOfCaller);
+                CalculatePossibleNeighbors(-1);
+                Debug.LogFormat("ONCOLLAPSE:{0} has these possible neighbors: {1},{2},{3},{4}.",this.name, possibleNeighboringSuperpositions[0].Count,
+                    possibleNeighboringSuperpositions[1].Count, possibleNeighboringSuperpositions[2].Count, 
+                    possibleNeighboringSuperpositions[3].Count);
                 return;
             }
         }
@@ -170,7 +172,7 @@ public class Tile : MonoBehaviour
                 if (neighbors[j] != null)
                 {
                     short s = (short)((j + 2) % 4);
-                    UpdateNeighborsSuperpositions(false, neighborCandidates[j], s);
+                    neighbors[j].UpdateNeighborsSuperpositions(false, possibleNeighboringSuperpositions[j], s);
                 }
             }
         }
@@ -184,26 +186,31 @@ public class Tile : MonoBehaviour
             {
                 if(n != ignoreIndex)
                 {
-                    for (int c = neighborCandidates[n].Count; c > 0; c--)
+                    if (!neighbors[n].collapsed)
                     {
-                        bool possible = false;
-
-                        for (int s = 0; s < superpositions.Count && !possible; s++)
+                        for (int c = possibleNeighboringSuperpositions[n].Count; c > 0; c--)
                         {
-                            short index = superpositions[s];
-                            short possibility = neighborCandidates[n][c - 1];
+                            bool possible = false;
 
-                            if (TilesMaster.allTiles[index].rules[n].Contains(possibility))
+                            for (int s = 0; s < superpositions.Count && !possible; s++)
                             {
-                                possible = true;
+                                TileInfo current = TilesMaster.allTiles[superpositions[s]];
+                                TileInfo neighbor = TilesMaster.allTiles[possibleNeighboringSuperpositions[n][c - 1]];
+
+                                if (WFC.IsNeighborValid(current.sockets, neighbor.sockets, n))
+                                {
+                                    possible = true;
+                                }
+                            }
+
+                            if (!possible)
+                            {
+                                Debug.LogFormat("REMOVED! {0} INDEX {1}: {2}. IgnoreIndex = {3}.", this.name, (c - 1), possibleNeighboringSuperpositions[n][c - 1], ignoreIndex);
+                                possibleNeighboringSuperpositions[n].RemoveAt(c - 1);
                             }
                         }
-
-                        if (!possible)
-                        {
-                            neighborCandidates[n].RemoveAt(c - 1);
-                        }
                     }
+                    
                 }
             }
         }
@@ -228,8 +235,67 @@ public class Tile : MonoBehaviour
         }
     }
 
-    void DoubleCheckSuperpositions()
+    bool DoubleCheckSuperposition(short index)
     {
+        TileInfo t = TilesMaster.allTiles[index];
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            if (neighbors[i] != null)
+            {
+                if (neighbors[i].collapsed)
+                {
+                    TileInfo n = TilesMaster.allTiles[neighbors[i].tileNum];
 
+                    if (!WFC.IsNeighborValid(t.sockets, n.sockets, i))
+                    {
+                        Debug.LogWarningFormat("{0} is <color=magenta>not valid</color> for {1}!", this.name, neighbors[i].name);
+                        this.collapsed = false;
+                        neighbors[i].collapsed = false;
+                        TilesMaster.collapsedTiles -= 2;
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void AssignStartingSuperpositions(int x, int y)
+    {
+        tileLoc = new Vector2Int(x, y);
+
+        if (y == WFC.yLen - 1)
+        {
+            if (x == 0 || x == WFC.xLen - 1)
+            {
+                superpositions = (x == 0 ? new List<short>() { 0, 2, 10 } :
+                    new List<short>() { 0, 4, 11 });
+            }
+            else
+            {
+                superpositions = new List<short> { 0, 2, 3, 4, 10, 11 };
+            }
+        }
+        else if (y == 0)
+        {
+            if (x == 0 || x == WFC.xLen - 1)
+            {
+                superpositions = (x == 0 ? new List<short>() { 0, 7, 12 } :
+                    new List<short>() { 0, 9, 13 });
+            }
+            else
+            {
+                superpositions = new List<short> { 0, 7, 8, 9, 12, 13 };
+            }
+        }
+        else
+        {
+            if (x == 0 || x == WFC.xLen - 1)
+            {
+                superpositions = (x == 0 ?
+                    new List<short>() { 0, 2, 5, 7, 10, 12 } :
+                    new List<short>() { 0, 4, 6, 9, 11, 13 });
+            }
+        }
     }
 }
--- Assets/Scripts/TilesMaster.cs
+++ Assets/Scripts/TilesMaster.cs
@@ -7,183 +7,117 @@ public static class TilesMaster
     public static TileInfo[] allTiles =
     {
         // 0: NEGATIVE SPACE!
-        new TileInfo(0, 10,
+        new TileInfo(0, 100,
             new short[] {-1,2,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1},
-            new List<short>{0,8,12,13},     // N = 0
-            new List<short>{0,2,5,7,10,12}, // E = 1
-            new List<short>{0,2,3,4,10,11}, // S = 2
-            new List<short>{0,4,6,9,11,13}),// W = 3
+            new bool[] {true, true, true, true}),
         // 1: JUST FLOOR!
-        new TileInfo(1,30,
+        new TileInfo(1,100,
             new short[] {-1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
-            new List<short>{1,3,16,17,18,19,20},
-            new List<short>{1,6,14,16,18,19,20},
-            new List<short>{1,8,14,15,18,19,20},
-            new List<short>{1,5,15,17,18,19,20}),
+            new bool[] {false, false, false, false}),
         // 2: NW CORNER
-        new TileInfo(2,25,
+        new TileInfo(2,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{0,7,8,9,12,13},
-            new List<short>{3,4,11,17},
-            new List<short>{5,7,12,17},
-            new List<short>{0,4,6,9,11,13}),
+            new bool[] {true, true, false, true}),
         // 3: NORTH WALL
-        new TileInfo(3,20,
+        new TileInfo(3,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{0,7,8,9,12,13},
-            new List<short>{3,4,11},
-            new List<short>{1,8,14,15,18,19,20},
-            new List<short>{2,3,10}),
+            new bool[] {true, true, false, false}),
         // 4: NE CORNER
-        new TileInfo(4,25,
+        new TileInfo(4,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{0,7,8,9,12,13},
-            new List<short>{0,2,5,7,10,12},
-            new List<short>{6,9,13,16},
-            new List<short>{2,3,10,16}),
+            new bool[] {true, true, true, false}),
         // 5: WEST WALL
-        new TileInfo(5,25,
+        new TileInfo(5,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{2,5,10,15},
-            new List<short>{1,6,14,16,18,19,20},
-            new List<short>{5,7,12,17},
-            new List<short>{0,4,6,9,11,13}),
+            new bool[] {true, false, false, true}),
         // 6: EAST WALL
-        new TileInfo(6,25,
+        new TileInfo(6,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{4,6,11,14},
-            new List<short>{0,2,5,7,10,12},
-            new List<short>{6,9,13,16},
-            new List<short>{1,5,15,17,18,19,20}),
+            new bool[] {false, true, true, false}),
         // 7: SW CORNER
-        new TileInfo(7,25,
+        new TileInfo(7,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{2,5,10,15},
-            new List<short>{8,9,13,15},
-            new List<short>{0,2,3,4,10,11},
-            new List<short>{0,4,6,9,11,13}),
+            new bool[] {true, false, true, true}),
         // 8: SOUTH WALL
-        new TileInfo(8,20,
+        new TileInfo(8,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{1,3,16,17,18,19,20},
-            new List<short>{8,9,13,15},
-            new List<short>{0,2,3,4,10,11},
-            new List<short>{7,8,12,14}),
+            new bool[] {false, false, true, true}),
         // 9: SE CORNER
-        new TileInfo(9,25,
+        new TileInfo(9,100,
             new short[] {0,2,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,1,1,1,1,2,2,2},
-            new List<short>{4,6,11,14},
-            new List<short>{0,2,5,7,10,12},
-            new List<short>{0,2,3,4,10,11},
-            new List<short>{7,8,12,14}),
+            new bool[] {false, true, true, true}),
         // 10: SOUTHEAST SLANT
-        new TileInfo(10,15,
+        new TileInfo(10,100,
             new short[] {1,1,1,1,1,0,0,0,0,0,-1,0,-1,-1,0,1,0,2,1,1,1},
-            new List<short>{0,7,8,9,12,13},
-            new List<short>{3,4,11,17},
-            new List<short>{5,7,12,17},
-            new List<short>{0,4,6,9,11,13}),
+            new bool[] {true, true, false, true}),
         // 11: SOUTHWEST SLANT
-        new TileInfo(11,15,
+        new TileInfo(11,100,
             new short[] {1,1,1,1,1,0,0,0,0,0,0,-1,-1,-1,0,0,2,1,1,1,1},
-            new List<short>{0,7,8,9,12,13},
-            new List<short>{0,2,5,7,10,12},
-            new List<short>{6,9,13,16},
-            new List<short>{2,3,10,16}),
+            new bool[] {true, true, true, false}),
         // 12: NORTHEAST SLANT
-        new TileInfo(12,15,
+        new TileInfo(12,100,
             new short[] {1,1,1,1,1,0,0,0,0,0,1,0,-1,1,0,1,0,2,1,1,1},
-            new List<short>{2,5,10,15},
-            new List<short>{8,9,13,15},
-            new List<short>{0,2,3,4,10,11},
-            new List<short>{0,4,6,9,11,13}),
+            new bool[] {true, false, true, true}),
         // 13: NORTHWEST SLANT
-        new TileInfo(13,15,
+        new TileInfo(13,100,
             new short[] {1,1,1,1,1,0,0,0,0,0,0,-1,-1,-1,0,0,2,1,1,1,1},
-            new List<short>{4,6,11,14},
-            new List<short>{0,2,5,7,10,12},
-            new List<short>{0,2,3,4,10,11},
-            new List<short>{7,8,12,14}),
+            new bool[] {false, true, true, true}),
         // 14: SOUTHEAST BRICKS
-        new TileInfo(14,10,
+        new TileInfo(14,100,
             new short[] {1,1,0,0,0,-1,1,1,1,1,2,0,0,2,-1,0,0,0,1,1,1},
-            new List<short>{1,3,16,17,18,19,20},
-            new List<short>{8,9,13,15},
-            new List<short>{6,9,13,16},
-            new List<short>{1,5,15,17,18,19,20}),
+            new bool[] {false, false, true, false}),
         // 15: SOUTHWEST BRICKS
-        new TileInfo(15,10,
+        new TileInfo(15,100,
             new short[] {},
-            new List<short>{1,3,16,17,18,19,20},
-            new List<short>{1,6,16,14,18,19,20},
-            new List<short>{5,7,12,17},
-            new List<short>{7,8,12,14}),
+            new bool[] {false, false, false, true}),
         // 16: NORTHEAST BRICKS
-        new TileInfo(16,10,
+        new TileInfo(16,100,
             new short[] {},
-            new List<short>{4,6,11,14},
-            new List<short>{3,4,11,17},
-            new List<short>{1,8,14,15,18,19,20},
-            new List<short>{1,5,15,17,18,19,20}),
+            new bool[] {false, true, false, false}),
         // 17: NORTHWEST BRICKS
-        new TileInfo(17,10,
+        new TileInfo(17,100,
             new short[] {},
-            new List<short>{2,5,10,15},
-            new List<short>{1,6,14,16,18,19,20},
-            new List<short>{1,8,14,15,18,19,20},
-            new List<short>{2,3,10,16}),
+            new bool[] {true, false, false, false}),
         // 18: BRICK FLOOR
-        new TileInfo(18,5,
+        new TileInfo(18,100,
             new short[] {},
-            new List<short>{1,3,16,17,19,20},
-            new List<short>{1,6,14,16,19,20},
-            new List<short>{1,8,14,15,19,20},
-            new List<short>{1,5,15,17,19,20}),
+            new bool[] {false, false, false, false}),
         // 19: HUMAN BONES!
-        new TileInfo(19,7,
+        new TileInfo(19,100,
             new short[] {},
-            new List<short>{1,3,16,17,18,20},
-            new List<short>{1,6,14,16,18,20},
-            new List<short>{1,8,14,15,18,20},
-            new List<short>{1,5,15,17,18,20}),
+            new bool[] {false, false, false, false}),
         // 20: MONSTER BONES!
-        new TileInfo(20,8,
+        new TileInfo(20,100,
             new short[] {},
-            new List<short>{1,3,16,17,18,19},
-            new List<short>{1,6,14,16,18,19},
-            new List<short>{1,8,14,15,18,19},
-            new List<short>{1,5,15,17,18,19})
+            new bool[] {false, false, false, false})
     };
 
     public static void EvaluatePercentages(short num, bool reverse=false)
     {
+        /*
         int a = reverse ? -1 : 1;
         for(int i = 0; i < TilesMaster.allTiles.Length; i++)
         {
             int b = (i == num) ? -1 : 1;
             TilesMaster.allTiles[i].percentage += (short)(a * b);
         }
-        
+        */
     }
 }
 
 public class TileInfo
 {
     public short tileIndex;
-    public List<short>[] rules = new List<short>[4];
+    public bool[] sockets = new bool[4];
     public short[] changes = new short[21];
     public short percentage;
 
-    public TileInfo(short index, short percent, short[] changelist, List<short> north, List<short> east, List<short> south, List<short> west)
+    public TileInfo(short index, short percent, short[] changelist, bool[] rules)
     {
         this.tileIndex = index;
         this.percentage = percent;
         this.changes = changelist;
-
-        if (north != null) { rules[0] = north; }
-        if (east  != null) { rules[1] = east ; }
-        if (south != null) { rules[2] = south; }
-        if (west  != null) { rules[3] = west ; }
+        this.sockets = rules;
     }
 }
 
