c715e74520fcfd0e0109fad6dc45e36d6776219d
chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
2023-04-13 12:10:24 -0400

    Created PlayerMove script to handle movement.
    Updated ShadowInfos to include physics.
    Also testing out colors.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 movementInput;
+    [SerializeField] float speed = 100;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal == 0 && vertical == 0)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        movementInput = new Vector2 (horizontal, vertical);
+        rb.velocity = movementInput * speed * Time.deltaTime;
+
+    }
+}
--- Assets/Scripts/TilesMaster.cs
+++ Assets/Scripts/TilesMaster.cs
@@ -12,55 +12,55 @@ public static class TilesMaster
             new bool[] {true, true, true, true}),
         // 1: JUST FLOOR!
         new TileInfo(1,100,
-            new short[] {0,-5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-2,-2,-2},
+            new short[] {0,-6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-2,-2,-2},
             new bool[] {false, false, false, false}),
         // 2: NW CORNER
         new TileInfo(2,100,
-            new short[] {-1,1,-3,-4,-4,-4,-2,-5,-4,-4,-2,-4,-3,-2,-2,-2,-2,-4,1,1,1},
+            new short[] {-1,1,-3,-4,-4,-4,-2,-5,-4,-4,-2,-4,-3,-2,-2,-2,-2,-4,1,2,2},
             new bool[] {true, true, false, true}),
         // 3: NORTH WALL
         new TileInfo(3,100,
-            new short[] {0,0,-4,-2,-4,-3,-3,-4,-4,-4,-3,-3,-3,-3,-3,-3,-3,-3,0,0,0},
+            new short[] {0,-1,-4,-2,-4,-3,-3,-4,-4,-4,-3,-3,-3,-3,-3,-3,-3,-3,0,0,0},
             new bool[] {true, true, false, false}),
         // 4: NE CORNER
         new TileInfo(4,100,
-            new short[] {-1,1,-4,-4,-3,-2,-4,-5,-4,-5,-4,-4,-4,-4,-2,-2,-4,-2,1,1,1},
+            new short[] {-1,1,-4,-4,-3,-2,-4,-5,-4,-5,-4,-4,-4,-4,-2,-2,-4,-2,1,2,2},
             new bool[] {true, true, true, false}),
         // 5: WEST WALL
         new TileInfo(5,100,
-            new short[] {0,0,-4,-3,-3,-4,-3,-4,-3,-4,-3,-3,-3,-3,-2,-3,-3,-3,0,0,0},
+            new short[] {0,-1,-4,-3,-3,-4,-3,-4,-3,-4,-3,-3,-3,-3,-2,-3,-3,-3,0,0,0},
             new bool[] {true, false, false, true}),
         // 6: EAST WALL
         new TileInfo(6,100,
-            new short[] {0,0,-4,-3,-3,-3,-4,-4,-3,-4,-3,-3,-3,-3,-3,-2,-3,-3,0,0,0},
+            new short[] {0,-1,-4,-3,-3,-3,-4,-4,-3,-4,-3,-3,-3,-3,-3,-2,-3,-3,0,0,0},
             new bool[] {false, true, true, false}),
         // 7: SW CORNER
         new TileInfo(7,100,
-            new short[] {-1,1,-4,-3,-5,-4,-4,-2,-4,-5,-4,-4,-2,-3,-2,-4,-2,-2,1,1,1},
+            new short[] {-1,1,-4,-3,-5,-4,-4,-2,-4,-5,-4,-4,-2,-3,-2,-4,-2,-2,1,2,2},
             new bool[] {true, false, true, true}),
         // 8: SOUTH WALL
         new TileInfo(8,100,
-            new short[] {0,0,-4,-4,-4,-3,-3,-4,-3,-3,-3,-3,-3,-3,-3,-3,-2,-3,0,0,0},
+            new short[] {0,-1,-4,-4,-4,-3,-3,-4,-3,-3,-3,-3,-3,-3,-3,-3,-2,-3,0,0,0},
             new bool[] {false, false, true, true}),
         // 9: SE CORNER
         new TileInfo(9,100,
-            new short[] {-1,1,-5,-3,-4,-4,-4,-5,-4,-2,-4,-4,-3,-2,-4,-2,-2,-2,1,1,1},
+            new short[] {-1,1,-5,-3,-4,-4,-4,-5,-4,-2,-4,-4,-3,-2,-4,-2,-2,-2,1,2,2},
             new bool[] {false, true, true, true}),
         // 10: SOUTHEAST SLANT
         new TileInfo(10,100,
-            new short[] {-2,1,-2,-3,-4,-3,-3,-4,-3,-4,-5,-5,-3,-4,-3,-3,-3,-5,1,1,1},
+            new short[] {-2,1,-2,-3,-4,-3,-3,-4,-3,-4,-5,-5,-3,-4,-3,-3,-3,-5,0,1,0},
             new bool[] {true, true, false, true}),
         // 11: SOUTHWEST SLANT
         new TileInfo(11,100,
-            new short[] {-2,1,-4,-3,-2,-3,-3,-4,-3,-4,-5,-5,-4,-3,-3,-3,-5,-3,1,1,1},
+            new short[] {-2,1,-4,-3,-2,-3,-3,-4,-3,-4,-5,-5,-4,-3,-3,-3,-5,-3,0,1,0},
             new bool[] {true, true, true, false}),
         // 12: NORTHEAST SLANT
         new TileInfo(12,100,
-            new short[] {-2,1,-4,-3,-4,-3,-3,-2,-3,-4,-3,-4,-5,-5,-3,-5,-3,-3,1,1,1},
+            new short[] {-2,1,-4,-3,-4,-3,-3,-2,-3,-4,-3,-4,-5,-5,-3,-5,-3,-3,0,0,1},
             new bool[] {true, false, true, true}),
         // 13: NORTHWEST SLANT
         new TileInfo(13,100,
-            new short[] {-2,1,-4,-3,-4,-3,-3,-4,-3,-2,-4,-3,-5,-5,-5,-3,-3,-3,1,1,1},
+            new short[] {-2,1,-4,-3,-4,-3,-3,-4,-3,-2,-4,-3,-5,-5,-5,-3,-3,-3,0,0,1},
             new bool[] {false, true, true, true}),
         // 14: SOUTHEAST BRICKS
         new TileInfo(14,100,
@@ -80,15 +80,15 @@ public static class TilesMaster
             new bool[] {true, false, false, false}),
         // 18: BRICK FLOOR
         new TileInfo(18,100,
-            new short[] { 2, 0, 1, 2, 1, 2, 2, 1, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, -20,-10,-10},
+            new short[] { 2, 0, 1, 2, 1, 2, 2, 1, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, -30,-15,-15},
             new bool[] {false, false, false, false}),
         // 19: HUMAN BONES!
         new TileInfo(19,100,
-            new short[] { 2, 0, 3, 1, 3, 0, 0, 3, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, -10,-20,-10},
+            new short[] { 2, 0, 3, 1, 3, 0, 0, 3, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, -15,-30,-15},
             new bool[] {false, false, false, false}),
         // 20: MONSTER BONES!
         new TileInfo(20,100,
-            new short[] { 2, 0, 1, 3, 1, 0, 0, 1, 3, 1, 2, 2, 2, 2, 1, 1, 1, 1, -10,-10,-20},
+            new short[] { 2, 0, 1, 3, 1, 0, 0, 1, 3, 1, 2, 2, 2, 2, 1, 1, 1, 1, -15,-15,-30},
             new bool[] {false, false, false, false})
     };
 
